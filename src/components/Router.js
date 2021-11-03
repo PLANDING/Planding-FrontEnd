2e5b8f33601ece2fee824bbdd9d2acc9f9400c21
runru1030 <runru1030@gmail.com>
2021-11-03 16:36:09 +0900

    [FEAT]: creation/detail progress/detail router Issue-#6

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,8 +2,10 @@ import react from "react";
 import { BrowserRouter, Switch, Route} from "react-router-dom";
 import Alert from "../routes/Alert";
 import FundingCompletion from "../routes/FundingCompletion";
+import FundingCompletionDetail from "../routes/FundingCompletionDetail";
 import FundingCreation from "../routes/FundingCreation";
 import FundingProgress from "../routes/FundingProgress";
+import FundingProgressDetail from "../routes/FundingProgressDetail";
 import Login from "../routes/Login";
 import Main from "../routes/Main";
 import Profile from "../routes/Profile";
@@ -18,7 +20,9 @@ const AppRouter=()=>{
                 <Route path="/login" component={Login}/>
                 <Route path="/register" component={Register}/>
                 <Route exact path="/progress" component={FundingProgress}/>
+                <Route exact path="/progress/detail" component={FundingProgressDetail}/>
                 <Route exact path="/completion" component={FundingCompletion}/>
+                <Route exact path="/completion/detail" component={FundingCompletionDetail}/>
                 <Route path="/creation" component={FundingCreation}/>
                 <Route path="/profile" component={Profile}/>
                 <Route path="/project" component={Project}/>
