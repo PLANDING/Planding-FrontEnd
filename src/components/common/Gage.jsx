bc3a4a3e8abaca1726f2dafd238bbd7daedd4d3a
runru1030 <runru1030@gmail.com>
2021-11-08 01:25:58 +0900

    [STYLE]: move gege number from FundingGage to Gage Issue-#20

--- src/components/common/Gage.jsx
+++ src/components/common/Gage.jsx
@@ -1,16 +1,28 @@
 import styled from "styled-components";
-const Gage=({gage, width})=>{
-    return(<GageContainer width={width} gage={gage}>
-        <div></div>
-    </GageContainer>)
+const Gage = ({ gage, width }) => {
+    return (
+        <Wrapper className="row-container">
+            <GageContainer width={width} gage={gage}>
+                <div></div>
+            </GageContainer>
+            <span>{gage}%</span>
+        </Wrapper>)
 }
-const GageContainer= styled.div`
+const Wrapper=styled.div`
+width:100%;
+justify-content: center;
+gap: 10px;
+color: #37C56E; 
+font-weight: bold;
+font-size: small;
+`
+const GageContainer = styled.div`
 background-color: #EDF2F8;
-width:${prop=>prop.width?prop.width:"300px"};
+width:${prop => prop.width ? prop.width : "300px"};
 height: 10px;
 &>div{
 background-color: #37C56E;
-width:${prop=>prop.gage&&prop.gage+"%"};
+width:${prop => prop.gage && prop.gage + "%"};
 height: 10px;
 }`
 export default Gage;
