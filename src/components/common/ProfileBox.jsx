bcc8f022d55645ee7008815fccfd1b38d8507f4a
runru1030 <runru1030@gmail.com>
2021-11-13 00:20:51 +0900

    [FEAT]: ProfileBox /profile 연결 Issue-#38

--- src/components/common/ProfileBox.jsx
+++ src/components/common/ProfileBox.jsx
@@ -1,12 +1,17 @@
+import { useHistory } from "react-router";
 import styled from "styled-components";
 import userImg from "../../assets/imgs/user.png"
 
 const ProfileBox = ({profileUrl, nickName, size}) => {
+    const history=useHistory();
+    const onClickProfile=()=>{
+        history.push("/profile");
+    }
     return (
-        <ProfileWrapper className="row-container" size={size}>
-            <div className="profile-img">
+        <ProfileWrapper className="row-container" size={size} onClick={onClickProfile}>
+            <ImgWrapper>
                 <img src={profileUrl?profileUrl:userImg} width="100%" />
-            </div>
+            </ImgWrapper>
             <span>{nickName}</span>
         </ProfileWrapper>
     )
@@ -14,17 +19,18 @@ const ProfileBox = ({profileUrl, nickName, size}) => {
 export default ProfileBox;
 
 const ProfileWrapper=styled.div`
-font-size: small;
-gap: 10px;
-.profile-img{
-overflow: hidden;
-${props=>props.size?
-`width:${props.size};
-height:${props.size};`
-:
-`width: 40px;
-height: 40px;`
-}
-border-radius: 30px;
-}
+    font-size: small;
+    gap: 10px;
+    cursor: pointer;
 ` 
+const ImgWrapper=styled.div`
+    overflow: hidden;
+    ${props=>props.size?
+    `width:${props.size};
+    height:${props.size};`
+    :
+    `width: 40px;
+    height: 40px;`
+    }
+    border-radius: 30px;
+`
