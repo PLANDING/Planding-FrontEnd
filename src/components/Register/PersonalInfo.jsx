dc6794a0408d912e2ab3b8919c42116b68864100
runru1030 <runru1030@gmail.com>
2021-11-13 00:22:03 +0900

    [DESIGN]:Login&Register DESIGN Issue-#38

--- src/components/Register/PersonalInfo.jsx
+++ src/components/Register/PersonalInfo.jsx
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import { Devider, InfoWrapper } from "../../pages/Register";
 import { GreenBorderBtn } from "../common/Button";
 
@@ -5,32 +6,33 @@ const PersonalInfo = ({registerInfo, onChangeInfo}) => {
     return (<>
         <Devider>
             <span>개인 정보</span>
-            <hr className="green-hr" />
+            <hr/>
         </Devider>
-        <InfoWrapper className="col-container wrapper">
-            <div className="row-container">
+        <InfoWrapper className="col-container">
+            <Wrapper className="row-container">
                 <span id="point">*</span>
                 <span id="label">아이디</span>
                 <input type="text" name="id" value={registerInfo.id} placeholder="이메일" onChange={onChangeInfo}/>
                 <GreenBorderBtn>중복확인</GreenBorderBtn>
-            </div>
-            <div className="row-container">
+            </Wrapper>
+            <Wrapper className="row-container">
                 <span id="point">*</span>
                 <span id="label">비밀번호</span>
                 <input type="password" name="pw" value={registerInfo.pw} placeholder="" onChange={onChangeInfo}/>
-            </div>
-            <div className="row-container">
+            </Wrapper>
+            <Wrapper className="row-container">
                 <span id="point">*</span>
                 <span id="label">비밀번호 확인</span>
                 <input type="password" name="pwCheck" value={registerInfo.pwCheck} placeholder="" onChange={onChangeInfo}/>
-            </div>
-            <div className="row-container">
+            </Wrapper>
+            <Wrapper className="row-container">
                 <span id="point">*</span>
                 <span id="label">닉네임</span>
                 <input type="text" name="nickName" value={registerInfo.nickName} placeholder="" onChange={onChangeInfo}/>
                 <GreenBorderBtn>중복확인</GreenBorderBtn>
-            </div>
+            </Wrapper>
         </InfoWrapper>
     </>)
 }
 export default PersonalInfo;
+const Wrapper=styled.div``
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,7 @@ import { GreenBorderBtn } from "../components/common/Button";
 import Header from "../components/common/Header";
 import LoginForm from "../components/Login/LoginForm";
 import TopDiv from "../components/common/TopDIv";
-
+import logoImg from "../assets/imgs/logo.png"
 const Login = () => {
     const history = useHistory();
     const onClickRegister = () => {
@@ -12,15 +12,13 @@ const Login = () => {
     }
     return (<>
         <Header />
-        <div className="login main-container">
+        <div className="main-container">
             <TopDiv pageLabel="로그인" />
             <Borderbox className="col-container">
-                <img src="logo.png" width="100px" />
-                <div className="wrapper col-container">
-
+                <img src={logoImg} width="100px" />
+                <div className="col-container">
                     <LoginForm />
                     <GreenBorderBtn onClick={onClickRegister}>회원가입</GreenBorderBtn>
-
                 </div>
             </Borderbox>
         </div>
@@ -28,18 +26,18 @@ const Login = () => {
 }
 export default Login;
 const Borderbox = styled.div`
-width: 50%;
-align-items: center;
-padding: 50px 0;
-margin-top: 50px;
-border: solid thin #37C56E;
-border-radius: 10px;
-&>div{
-width: 50%;
-margin-top: 50px;
-&>button{
-  margin-top: 50px;
-  align-self: flex-end;
-}
-}
+    width: 50%;
+    align-items: center;
+    padding: 50px 0;
+    margin-top: 50px;
+    border: solid thin #37C56E;
+    border-radius: 10px;
+    &>div{
+        width: 50%;
+        margin-top: 50px;
+        &>button{
+            margin-top: 50px;
+            align-self: flex-end;
+        }
+    }
 `
