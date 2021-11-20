332aee4d2d1adedab54f8e20406871be1abbd5c5
runru1030 <runru1030@gmail.com>
2021-11-21 00:05:28 +0900

    [FEAT]: login & 자동 login Issue-#46

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,32 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import '../App.css';
+import { setLoggedInfo } from '../modules/user';
 import Footer from './common/Footer';
 import AppRouter from './Router';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    sendJwtTokenToServer();
+  }, []);
+  /* 자동 로그인 */
+  const sendJwtTokenToServer = () => {
+    axios.post('/auth')
+      .then(res => {
+        if (res.status == 200) {
+          console.log(res.data);
+          dispatch(setLoggedInfo(true, res.data.user));
+
+        }
+        else {
+          //자동 로그인 실패
+          dispatch(setLoggedInfo(false, null));
+        }
+      })
+  }
   return (<>
     <AppRouter />
     <Footer />
--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,6 +1,7 @@
 import { faBell } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
 import { useHistory, useLocation } from 'react-router';
 import styled from 'styled-components';
 import logoSrc from '../../assets/imgs/logo.png';
@@ -9,14 +10,17 @@ import { GreenBorderBtn } from './Button';
 import Modal from './Modal';
 import ProfileBox from './ProfileBox';
 const Header = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
     const location = useLocation();
     const history = useHistory();
+
+    const isLoggedIn = useSelector(state => ({ isLoggedIn: state.user.isLoggedIn }));
+
     const onClickNav = (e) => {
         history.push("/" + e.target.id);
     }
 
     const [isOpenModal, setIsOpenModal] = useState(false);
+
     const onClickAlert = () => {
         setIsOpenModal(prev => !prev);
     }
@@ -25,19 +29,19 @@ const Header = () => {
             <img src={logoSrc} width="100px" />
             <span>당신의 아이디어에 펀딩합니다</span>
         </LogoWrapper>
+
         <Nav className="row-container">
-            <button onClick={onClickNav} id="completion" className={location.pathname == "/completion" && "cur"}>펀딩 완료</button>
-            <button onClick={onClickNav} id="progress" className={location.pathname == "/progress" && "cur"}>펀딩 진행</button>
-            <button onClick={onClickNav} id="project" className={location.pathname == "/project" && "cur"}>나의 프로젝트</button>
-            {isLoggedIn ? <>
-                <button>
-                    <FontAwesomeIcon id="bell" icon={faBell} color={"#37C56E"} onClick={onClickAlert} />
-                    <Modal setIsOpen={setIsOpenModal}>
-                        {isOpenModal && <AlertModal />}
-                    </Modal>
-                </button>
-                <ProfileBox onClick={onClickNav} id="profile" />
-            </>
+            <Button onClick={onClickNav} id="completion" cur={location.pathname == "/completion" }>펀딩 완료</Button>
+            <Button onClick={onClickNav} id="progress" cur={location.pathname == "/progress"}>펀딩 진행</Button>
+            <Button onClick={onClickNav} id="project" cur={location.pathname == "/project"}>나의 프로젝트</Button>
+            {isLoggedIn ?
+                <>
+                    <AlertButton>
+                        <FontAwesomeIcon id="bell" icon={faBell} color={"#37C56E"} onClick={onClickAlert} />
+                        <Modal setIsOpen={setIsOpenModal}>{isOpenModal && <AlertModal />}</Modal>
+                    </AlertButton>
+                    <ProfileBox onClick={onClickNav} id="profile" />
+                </>
                 :
                 <GreenBorderBtn onClick={onClickNav} id="login">로그인</GreenBorderBtn>}
         </Nav>
@@ -45,28 +49,32 @@ const Header = () => {
 }
 export default Header;
 const Container = styled.div`
-height: 100px;
+    height: 100px;
 `
 const LogoWrapper = styled.div`
-margin-left: 50px;
-cursor: pointer;
-span{
-font-size: small;
-font-weight: lighter;
-align-self: flex-end;
-margin-bottom: 10px;
-}
+    margin-left: 50px;
+    cursor: pointer;
+    span{
+        font-size: small;
+        font-weight: lighter;
+        align-self: flex-end;
+        margin-bottom: 10px;
+    }
 `
 const Nav = styled.nav`
-gap: 50px;
-flex: 1;
-justify-content: right;
-margin-right: 50px;
-.cur{
-color: #37C56E;
-font-weight: bold;
-}
-#bell{
-    font-size: x-large;
-}
+    gap: 50px;
+    flex: 1;
+    justify-content: right;
+    margin-right: 50px;
+`
+const Button=styled.button`
+    ${props=>props.cur&&`
+        color: #37C56E;
+        font-weight: bold;
+    `}
+`
+const AlertButton=styled.button`
+    #bell{
+        font-size: x-large;
+    }
 `
