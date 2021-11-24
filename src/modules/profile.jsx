4c4770380f4ae5e51311bfc191caa8fd45b86e92
runru1030 <runru1030@gmail.com>
2021-11-25 00:48:50 +0900

    [FEAT]: profile reducer Issue-#50

---
+++ src/modules/profile.jsx
@@ -0,0 +1,25 @@
+const SET_PROFILE_INFO = 'profile/SET_PROFILE_INFO'; 
+
+
+const initialState = {
+    profileObj: {
+        id: 0,
+        nickName: "",
+      },
+  };
+
+export const setProfileInfo= (profileObj) => {
+    return {
+        type: SET_PROFILE_INFO,
+        profileObj
+    };
+};
+
+export default  function profile(state = initialState, action) {
+    switch (action.type) {
+        case SET_PROFILE_INFO:
+            return { ...state,profileObj: action.profileObj  };
+        default:
+            return state;
+    }
+};
