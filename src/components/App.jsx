98e8de76b27cd2026037697dfdbf2351a7fe9f18
runru1030 <runru1030@gmail.com>
2021-11-25 20:56:58 +0900

    [FIX]:자동 로그인 token 수정  Issue-#46

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { useEffect } from 'react';
+import { Cookies } from 'react-cookie';
 import { useDispatch } from 'react-redux';
 import '../App.css';
 import { setLoggedInfo } from '../modules/user';
@@ -8,9 +9,12 @@ import AppRouter from './Router';
 
 function App() {
   const dispatch = useDispatch();
-
+  const cookie=new Cookies;
   useEffect(() => {
-    sendJwtTokenToServer();
+    cookie.get('token')?
+    sendJwtTokenToServer()
+    :
+    dispatch(setLoggedInfo(false, null));
   }, []);
   /* 자동 로그인 */
   const sendJwtTokenToServer = () => {
@@ -23,6 +27,7 @@ function App() {
         }
         else {
           //자동 로그인 실패
+          console.log("none");
           dispatch(setLoggedInfo(false, null));
         }
       })
