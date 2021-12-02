24893222dcdd71db03d04a88e4b35a798c635100
runru1030 <runru1030@gmail.com>
2021-12-02 21:14:48 +0900

    [FEAT] : funding progress project -sever

--- src/components/FundingProgress/ProgressCard.jsx
+++ src/components/FundingProgress/ProgressCard.jsx
@@ -1,41 +1,56 @@
+import axios from "axios";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router";
 import styled from "styled-components";
+import { setProjectInfo } from "../../modules/project";
 import AcceptBtnBox from "../Alert/AcceptBtnBox";
 import Card, { ProjectHead, Wrapper } from "../common/Card";
 import FundingBtnBox from "../common/FundingBtnBox";
 import FundingGage from "../common/FundingGage";
 import InterestBox from "../common/InterestBox";
 import ProfileBox from "../common/ProfileBox";
-const ProgressCard = ({ projectObj, usage , idx}) => {
+const ProgressCard = ({ projectObj, usage, idx }) => {
     const history = useHistory();
+    const dispatch = useDispatch();
     const onClickCard = () => {
-        history.push("/progress/detail");
+        axios.get(`/project/progress/detail/${projectObj.id}`)
+            .then(res => {
+                dispatch(setProjectInfo(res.data.project));
+                history.push("/progress/detail");
+            })
     }
-    return (<Card border onClick={onClickCard} id={idx}>
-        <ProjectHead label={projectObj.isEnd ? "펀딩 마감" : "펀딩진행 중"} idea={projectObj.idea}>
-        <ProfileBox  nickName={projectObj.User.nickName} />
+    const ms = new Date().getTime() - new Date(projectObj.createdAt).getTime();
+    const date = Math.ceil(ms / (1000 * 3600 * 24));
+
+    const {isLoggedin, userObj}=useSelector(state=>({isLoggedin:state.user.isLoggedin, userObj:state.user.userObj}));
+    const [isFunding, setIsFunding] = useState(isLoggedin?projectObj.Fundings.findIndex(i => i.User.id == userObj.id) != -1:false);
+
+    return (<Card border id={idx}>
+        <ProjectHead label={date > 7 ? "펀딩 마감" : "펀딩진행 중"} idea={projectObj.idea} onClickCard={onClickCard}>
+            <ProfileBox nickName={projectObj.User.nickName} userId={projectObj.User.id} profileUrl={projectObj.User.ProfileImg?.url} />
         </ProjectHead>
-        <div className="row-container">
-            <Wrapper className="col-contaienr main-wrapper">
+        <div className="row-container" >
+            <Wrapper className="col-contaienr main-wrapper" onClick={onClickCard}>
                 <CategoryWrapper className="row-container">
                     <span>카테고리</span><span>{">"}</span>
-                    <span>{projectObj.Category.name}</span>
+                    <span>{projectObj.Category?.name}</span>
                 </CategoryWrapper>
 
                 <InterestBox interestArr={projectObj.Interests} />
 
-                <FundingGage gage={80} fundingCnt={1200} />
+                <FundingGage gage={(projectObj.Fundings.length * 500 / 30).toFixed(1)} fundingCnt={projectObj.Fundings.length * 500} />
             </Wrapper>
 
-            {usage!="isNone"&&(usage=="isAlert"?<AcceptBtnBox dDay={3} />:<FundingBtnBox dDay={3} />)}
-            
+            {usage != "isNone" && (usage == "isAlert" ? <AcceptBtnBox dDay={7 - date} /> : <FundingBtnBox dDay={7 - date} projectId={projectObj.id} userId={userObj?.id} isfunding={isFunding} />)}
+
         </div>
     </Card>);
 }
 export default ProgressCard;
 
 const CategoryWrapper = styled.div`
-span:nth-child(3){
-  font-weight: normal;
-}
+    span:nth-child(3){
+    font-weight: normal;
+    }
 `
--- src/components/common/FundingBtnBox.jsx
+++ src/components/common/FundingBtnBox.jsx
@@ -1,15 +1,60 @@
-import GreenBtn from "./Button";
-import { BtnBox } from "./JoinBtnBox";
-
-const FundingBtnBox = ({ dDay }) => {
+import axios from "axios";
+import { useState } from "react";
+import styled from "styled-components";
+import GreenBtn, { GrayBtn } from "./Button";
+const FundingBtnBox = ({ dDay, projectId, userId, isfunding, content, isRow }) => {
+    const [isFunding, setIsFunding] = useState(isfunding);
+    const onClickFunding = () => {
+        isFunding ?
+            axios.delete(`/funding/${projectId}/${userId}`)
+                .then(res => res.status == 200 && setIsFunding(p => !p))
+            :
+            axios.get(`/funding/${projectId}/${userId}`)
+                .then(res => res.status == 200 && setIsFunding(p => !p))
+    }
     return (
         <BtnBox className="col-container">
-            <div className="col-container">
-                <span id="d-day">D-{dDay}</span>
-                <GreenBtn className="green-btn">펀딩 하기</GreenBtn>
-            </div>
+            <Wrapper isRow={isRow}>
+                <span>{content}<Dday>D-{dDay}</Dday></span>
+                {isFunding ? <GrayBtn onClick={onClickFunding}>펀딩 취소</GrayBtn> : <GreenBtn onClick={onClickFunding}>펀딩 하기</GreenBtn>}
+            </Wrapper>
         </BtnBox>
     )
 
 }
 export default FundingBtnBox;
+const Wrapper = styled.div`   
+    gap: 10px;
+    align-items: center;
+    button{
+        padding: 10px 20px;
+        font-size: medium;
+    }
+    display: flex;
+    flex-direction: column;
+    ${props => props.isRow && `
+    flex-direction: row;
+    button{
+        padding: 5px 20px;
+    }
+    gap:15px;
+    margin-bottom: 20px;
+    &>span{
+        font-weight: bold;
+        color: #5F5F5F;
+    }`}
+`
+export const BtnBox = styled.div`
+    z-index: 99;
+    align-items: flex-end;
+    flex: 1;
+    align-self: flex-end;
+    span{
+        text-align: center;
+        font-weight: bold;
+    }
+`
+const Dday = styled.span`
+    color: #37C56E;
+    margin-left: 5px;
+`
