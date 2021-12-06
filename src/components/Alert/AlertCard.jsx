92e1281ea4eada0079530064dc4070051c5a3cbe
runru1030 <runru1030@gmail.com>
2021-12-06 17:09:06 +0900

    [FEAT]: alert list 받아오기 -server

--- src/components/Alert/AlertCard.jsx
+++ src/components/Alert/AlertCard.jsx
@@ -1,3 +1,6 @@
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react/cjs/react.development";
 import styled from "styled-components";
 import DateBox from "../common/DateBox";
 import ProfileBox from "../common/ProfileBox";
@@ -5,29 +8,27 @@ import CompletionCard from "../FundingCompletion/CompletionCard";
 import ProgressCard from "../FundingProgress/ProgressCard";
 
 const AlertCard = ({ content, date, projectId, fromUser }) => {
-    /*dummy data */
-    const projectObj = {
-        idea: "이미지 인식을 활용한 앱 서비스",
-        isCompletion: false,
-        isEnd: true,
-        User: { nickName: "닉네임" },
-        Interests: [{ name: "안드로이드" }, { name: "데이터 분석" }, { name: "앱 서버" }],
-        Category: { name: "인공지능" },
-        //+ funding count
-    }
+    const [projectObj, setProjectObj] = useState();
+    useEffect(() => {
+        axios.get(`/project/card/${projectId}`)
+            .then(res => { setProjectObj(res.data.project) });
+    }, []);
+
     return (
         <Container className="col-container">
             <Content className="row-container">
                 {fromUser ?
                     <><ProfileBox profileUrl={fromUser.ProfileImg?.url} />'{fromUser?.nickName}'{content}</>
                     : content}
-                    
+
                 <DateBox dateString={date} side />
             </Content>
-            
-            {projectObj.isCompletion ?
-                <CompletionCard projectObj={projectObj} usage={fromUser ? "isAlert" : "isNone"} /> :
-                <ProgressCard projectObj={projectObj} usage={fromUser ? "isAlert" : "isNone"} />}
+
+            {projectObj &&
+                (projectObj.isCompletion ?
+                    <CompletionCard projectObj={projectObj} usage={fromUser ? "isAlert" : "isNone"} />
+                    :
+                    <ProgressCard projectObj={projectObj} usage={fromUser ? "isAlert" : "isNone"} />)}
         </Container>
     )
 }
