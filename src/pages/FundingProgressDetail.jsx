effb830787a25fbeb9fffc2de076f04f77d4906c
runru1030 <runru1030@gmail.com>
2021-12-11 00:27:31 +0900

    [FEAT] : funding 후, 프로젝트 update & isCompletion

--- src/pages/FundingProgressDetail.jsx
+++ src/pages/FundingProgressDetail.jsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react/cjs/react.development";
 import styled from "styled-components";
-import GreenBtn from "../components/common/Button";
 import { ProjectHead } from "../components/common/Card";
 import FundingBtnBox from "../components/common/FundingBtnBox";
 import FundingGage from "../components/common/FundingGage";
@@ -15,26 +14,27 @@ import ContentBox from "../components/FundingCompletionDetail/ContentBox";
 import { setProjectInfo } from "../modules/project";
 const FundingProgressDetail = () => {
     const dispatch = useDispatch();
+
+    const { userObj } = useSelector(state => ({ userObj: state.user.userObj }));
     const { projectObj } = useSelector(state => ({ projectObj: state.project.projectObj }));
     const ms = new Date().getTime() - new Date(projectObj.createdAt).getTime();
     const date = Math.ceil(ms / (1000 * 3600 * 24));
 
-
-    const {userObj}=useSelector(state=>({userObj:state.user.userObj}));
     const [isFunding, setIsFunding] = useState(projectObj.Fundings.findIndex(i => i.User.id == userObj.id) != -1);
     useEffect(() => {
         axios.get(`/project/progress/detail/${projectObj.id}`)
             .then(res => {
                 dispatch(setProjectInfo(res.data.project));
             });
-    }, []);
+    }, [isFunding]);
+
     return (<>
         <Header />
         <div className="main-container">
             <TopDiv pageLabel="펀딩 진행" subLabel="프로젝트에 펀딩하세요!" isGreen />
-            <ProjectHead label={projectObj.isEnd? "펀딩 마감" : "펀딩진행 중"} idea={projectObj.idea} headilne={projectObj.headline} width="80%" isDetail>
+            <ProjectHead label={projectObj.isEnd ? "펀딩 마감" : "펀딩진행 중"} idea={projectObj.idea} headilne={projectObj.headline} width="80%" isDetail>
                 <SideBtnBox className="col-container">
-                    <FundingBtnBox dDay={7 - date} projectId={projectObj.id} userId={userObj.id} isfunding={isFunding} content="펀딩 종료까지" isRow/>
+                    <FundingBtnBox dDay={7 - date} projectId={projectObj.id} userId={userObj.id} isFunding={isFunding} setIsFunding={setIsFunding} content="펀딩 종료까지" isRow />
                     <FundingGage gage={(projectObj.Fundings.length * 500 / 30).toFixed(1)} fundingCnt={projectObj.Fundings.length * 500} width={"200px"} />
                 </SideBtnBox>
             </ProjectHead>
