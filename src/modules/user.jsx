efef7a91c2b0bd6d973187f5d4f7c4ad23ea4bd5
runru1030 <runru1030@gmail.com>
2021-12-16 17:09:34 +0900

    [FEAT] : user gaObj redux 속성 추가

--- src/modules/user.jsx
+++ src/modules/user.jsx
@@ -1,14 +1,19 @@
 const SET_LOGGED_INFO = 'user/SET_LOGGED_INFO'; // 로그인 정보 설정
-const SET_USER_INFO = 'user/SET_USER_INFO'; 
+const SET_USER_INFO = 'user/SET_USER_INFO';
+const SET_GA_INFO = 'user/SET_GA_INFO';
 
 
 const initialState = {
     userObj: {
         id: 0,
         nickName: "",
-      },
+    },
+    gaObj: {
+        category: "",
+        interests: []
+    },
     isLoggedin: false,
-  };
+};
 export const setLoggedInfo = (isLoggedin, userObj) => {
     return {
         type: SET_LOGGED_INFO,
@@ -17,22 +22,31 @@ export const setLoggedInfo = (isLoggedin, userObj) => {
     };
 };
 
-export const setUserInfo= (userObj) => {
+export const setUserInfo = (userObj) => {
     return {
         type: SET_USER_INFO,
         userObj
     };
 };
 
-export default  function user(state = initialState, action) {
+export const setGaInfo = (gaObj) => {
+    return {
+        type: SET_GA_INFO,
+        gaObj
+    };
+};
+export default function user(state = initialState, action) {
     switch (action.type) {
         case SET_LOGGED_INFO:
-            return {...state,
-                userObj:action.userObj,
+            return {
+                ...state,
+                userObj: action.userObj,
                 isLoggedin: action.isLoggedin
-                };
+            };
         case SET_USER_INFO:
-            return { ...state,userObj: action.userObj  };
+            return { ...state, userObj: action.userObj };
+        case SET_GA_INFO:
+            return { ...state, gaObj: action.gaObj };
         default:
             return state;
     }
