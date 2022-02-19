87c90f04e0dd56dd8f98b382cef38454a233d862
runru1030 <runru1030@gmail.com>
2022-02-19 22:12:14 +0900

    [FEAT & REFACT] : apply register reducer to profileEdit

---
+++ src/components/ProfileEdit/ProfileImgForm.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
+import profile from '../../assets/imgs/user.png';
+import { setRegsiterInfo } from '../../modules/register';
+import { GreenBorderLabel } from '../common/Label';
+import ProfileBox from '../common/ProfileBox';
+
+const ProfileImgForm = () => {
+  const { registerInfo } = useSelector((state) => state.register);
+  const { userObj } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const [imgFile, setImgFile] = useState({
+    fileURL: userObj.ProfileImg?.url,
+  });
+  const handlerChange = (e) => {
+    e.preventDefault();
+    const file = e.target.files[0];
+    dispatch(setRegsiterInfo({ ...registerInfo, profileImg: e.target.files[0] }));
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onloadend = () => {
+      const fileURL = reader.result;
+      setImgFile({ ...imgFile, file, fileURL });
+    };
+  };
+  return (
+    <Container className="col-container">
+      <ProfileBox
+        profileUrl={imgFile.fileURL ? imgFile.fileURL : profile}
+        size={'150px'}
+        borderNone
+      />
+      <label htmlFor="inputImg">
+        <GreenBorderLabel>이미지 선택</GreenBorderLabel>
+      </label>
+      <input type="file" id="inputImg" onChange={handlerChange} />
+    </Container>
+  );
+};
+
+export default ProfileImgForm;
+
+const Container = styled.div`
+  flex-grow: 1;
+  gap: 20px;
+  width: 150px;
+  align-items: center;
+  input {
+    display: none;
+  }
+  label {
+    width: 100%;
+  }
+  label > div {
+    text-align: center;
+    cursor: pointer;
+  }
+`;
--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,48 +1,32 @@
-import React, { useState } from 'react';
-import Header from '../components/common/Header';
-import PersonalInfo from '../components/ProfileEdit/PersonalInfo';
-import styled from 'styled-components';
-import SkillInfo from '../components/ProfileEdit/SkillInfo';
-import TopDiv from '../components/common/TopDIv';
-import GreenBtn from '../components/common/Button';
+import axios from 'axios';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
-import axios from 'axios';
+import styled from 'styled-components';
+import GreenBtn from '../components/common/Button';
+import Header from '../components/common/Header';
+import TopDiv from '../components/common/TopDIv';
+import PersonalInfo from '../components/ProfileEdit/PersonalInfo';
+import SkillInfo from '../components/Register/SkillInfo';
 
 const ProfileEdit = () => {
+  const { registerInfo, interestArr, skillArr, checkInfo } = useSelector((state) => state.register);
+  const { userObj } = useSelector((state) => state.user);
+
   const history = useHistory();
-  const { userObj } = useSelector((state) => ({ userObj: state.user.userObj })); //기존 user 정보
-
-  const [profileObj, setProfileObj] = useState(userObj); //수정용 user 정보
-  const [interestArr, setInterestArr] = useState(
-    userObj.Interests.map((interest) => interest.category + ':' + interest.name),
-  );
-  const [skillArr, setSkillArr] = useState(userObj.Skills.map((skill) => skill.name));
-  const [checkNick, setCheckNick] = useState('');
-
-  const onChangeInfo = (e) => {
-    const {
-      target: { name, value },
-    } = e;
-
-    setProfileObj((prev) => ({ ...prev, [name]: value }));
-    /* 중복확인 및 일치 여부 판별 */
-    name == 'nickName' && setCheckNick('');
-  };
-
   const onSubmit = (e) => {
     e.preventDefault();
     const formData = new FormData();
     try {
-      if (userObj.nickName != profileObj.nickName && checkNick != '사용 가능') {
+      if (userObj.nickName != registerInfo.nickName && !checkInfo.nickName) {
         throw new Error('닉네임 중복확인 해주세요.');
       }
-      formData.append('nickName', profileObj.nickName);
-      formData.append('site', profileObj.site);
-      formData.append('github', profileObj.github);
+      formData.append('nickName', registerInfo.nickName);
+      formData.append('site', registerInfo.site);
+      formData.append('github', registerInfo.github);
       formData.append('skillArr', skillArr);
       formData.append('interestArr', interestArr);
-      formData.append('profileImg', profileObj.profileImg);
+      formData.append('profileImg', registerInfo.profileImg);
       axios.patch(`/user/${userObj.id}`, formData).then((res) => {
         res.status == 200 && history.push('/profile');
       });
@@ -56,21 +40,8 @@ const ProfileEdit = () => {
       <div className="main-container">
         <TopDiv pageLabel={'프로필 수정'} />
         <Form className="main-container" onSubmit={onSubmit}>
-          <PersonalInfo
-            profileObj={profileObj}
-            setProfileObj={setProfileObj}
-            onChangeInfo={onChangeInfo}
-            checkNick={checkNick}
-            setCheckNick={setCheckNick}
-          />
-          <SkillInfo
-            profileObj={profileObj}
-            onChangeInfo={onChangeInfo}
-            interestArr={interestArr}
-            setInterestArr={setInterestArr}
-            skillArr={skillArr}
-            setSkillArr={setSkillArr}
-          />
+          <PersonalInfo />
+          <SkillInfo />
           <GreenBtn>수정 완료</GreenBtn>
         </Form>
       </div>
@@ -81,12 +52,11 @@ const ProfileEdit = () => {
 export default ProfileEdit;
 
 const Form = styled.form`
+  width: 50%;
+  margin-top: 50px;
   gap: 30px;
   input {
     flex-grow: 1;
-  }
-  #label {
-    width: 150px;
   }
   & > button {
     width: 30%;
