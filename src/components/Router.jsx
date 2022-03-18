c5de4607e35c1455c1894fef3c702a2ebd2107b9
Huiseon Jeon <huiseon37@gmail.com>
2022-03-18 19:24:54 +0900

    bug: prettier 적용

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -23,7 +23,7 @@ const AppRouter = () => {
         <Route path="/register" component={Register} />
         <Route exact path="/progress" component={FundingProgress} />
         <Route exact path="/progress/detail/:projectId" component={FundingProgressDetail} />
-        <Route exact path="/completion"component={FundingCompletion} />
+        <Route exact path="/completion" component={FundingCompletion} />
         <Route exact path="/completion/detail/:projectId" component={FundingCompletionDetail} />
         <Route path="/modification" component={FundingEdit} />
         <Route path="/creation" component={FundingCreation} />
